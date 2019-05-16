6b3e756dbe4a5ffaae9a6992e4d374cea8decbc2
Administrator <Administrator@DESKTOP-7E3R07U>
2019-05-16 14:01:03 +0800

    增加注释

--- run_all.py
+++ run_all.py
@@ -10,26 +10,20 @@ import time
 
 
 global report_path
+global case_path
 
 now=time.strftime("%Y-%m-%d_%H_%M_%S")
-report_path = './Report/'+now+'_result.html'
-case_path='./case/'
+report_path = './Report/'+'学科网自动化回归_'+now+'_result.html'  #报告路径
+case_path='./case/'  #用例路径
 
-# 用例路径
-#case_path = os.path.join(os.getcwd(), "case")
-# 报告存放路径
-
+#discover组装用例
 def all_case():
     discover = unittest.defaultTestLoader.discover(case_path,
                                                     pattern="test*.py",
                                                     top_level_dir=None)
-#     print(discover)
     return discover
 
-
-
 if __name__ == "__main__":
- 
     fp = open(report_path, "wb")
     runner = HTMLTestRunner_cn.HTMLTestRunner(title="测试报告", 
                                               description="学科网首页", 
